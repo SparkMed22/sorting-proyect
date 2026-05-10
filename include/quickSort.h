405560ae7cef5db5dee60b8c3c26914b3ece111a
Richard Parker <gonzaloenriquesolis21@gmail.com>
2026-05-10 19:12:42 -0300

    Update quickSort.h

--- include/quickSort.h
+++ include/quickSort.h
@@ -1,4 +1,11 @@
-/**
- * Implementa tu  algoritmo de ordenacion
- * 
- */
+#ifndef QUICKSORT_H   
+#define QUICKSORT_H   
+
+#include "listas.h"   
+void quickSortArray(int *datos, int cant);
+
+void quickSortArrayList(ArrayList *list, int cant);
+
+void quickSortLinkedList(LinkedList *list, int cant);
+
+#endif 
