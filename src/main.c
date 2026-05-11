d848d60db1fba685e47055c6c9c4c691a3bd25fb
Richard Parker <gonzaloenriquesolis21@gmail.com>
2026-05-11 11:37:31 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -6,6 +6,9 @@
 #include "radixSort.h"
 #include "util.h"
 
+#include "quickSort.h" 
+
+
 // Tamaños de prueba definidos según tus implementaciones previas
 static int sizes[] = {100, 1000, 5000, 10000, 50000, 100000, 500000, 1000000,2000000};
 static int num_sizes = 9;
@@ -17,7 +20,8 @@ void ejecutar_comparativa(const char* nombre_archivo, int tipo_dato) {
         return;
     }
 
-    fprintf(file, "size,heap_array,radix_array,heap_linkedlist,radix_linkedlist\n");
+    //Se añadio quick_array y quick_linkedlist
+    fprintf(file, "size,heap_array,radix_array,quick_array,heap_linkedlist,radix_linkedlist,quick_linkedlist\n");
     printf("\n--- Comparativa: %s ---\n", nombre_archivo);
 
     for (int i = 0; i < num_sizes; i++) {
@@ -43,6 +47,13 @@ void ejecutar_comparativa(const char* nombre_archivo, int tipo_dato) {
         radixSortArray(arr_radix, n);
         double t_radix_arr = (double)(clock() - start) / CLOCKS_PER_SEC;
 
+        // --- Benchmark QuickSort (Array) ---
+        int *arr_quick = malloc(n * sizeof(int));
+        for(int j=0; j<n; j++) arr_quick[j] = base[j];
+        start = clock();
+        quickSortArray(arr_quick, n);
+        double t_quick_arr = (double)(clock() - start) / CLOCKS_PER_SEC;
+
         // --- Benchmark HeapSort (LinkedList) ---
         LinkedList *list_heap = linkedlist_create();
         for(int j=0; j<n; j++) linkedlist_add(list_heap, base[j]);
@@ -57,6 +68,13 @@ void ejecutar_comparativa(const char* nombre_archivo, int tipo_dato) {
         radixSortLinkedList(list_radix,n);
         double t_radix_ll = (double)(clock() - start) / CLOCKS_PER_SEC;
 
+        // --- Benchmark QuickSort (LinkedList) ---
+        LinkedList *list_quick = linkedlist_create();
+        for(int j=0; j<n; j++) linkedlist_add(list_quick, base[j]);
+        start = clock();
+        quickSortLinkedList(list_quick, n);
+        double t_quick_ll = (double)(clock() - start) / CLOCKS_PER_SEC;
+
         // Guardar y mostrar resultados
         fprintf(file, "%d, %f, %f, %f, %f\n", n, t_heap_arr, t_radix_arr, t_heap_ll, t_radix_ll);
         printf("n=%7d | Heap: %f | Radix: %f (Array)\n", n, t_heap_arr, t_radix_arr);
@@ -65,8 +83,10 @@ void ejecutar_comparativa(const char* nombre_archivo, int tipo_dato) {
         free(base);
         free(arr_heap);
         free(arr_radix);
+        free(arr_quick);
         linkedlist_free(list_heap);
         linkedlist_free(list_radix);
+        linkedlist_free(list_quick);
     }
     fclose(file);
 }
